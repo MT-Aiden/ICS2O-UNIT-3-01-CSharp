a0e0f9152ebaa1bbf605aa9e04043754e1dfd826
MT-Aiden <99355180+MT-Aiden@users.noreply.github.com>
2022-04-19 01:04:58 +0000

    main

--- main.cs
+++ main.cs
@@ -1,7 +1,29 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        int basea;
+        int baseb;
+        int height;
+        int area;
+
+        Console.WriteLine("Please enter the both bases and height.");
+        Console.WriteLine("");
+
+        Console.Write("Base A: ");
+        basea = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Base B: ");
+        baseb = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Height: ");
+        height = Convert.ToInt32(Console.ReadLine());
+        area = ((basea + baseb) / 2) * height;
+
+        Console.WriteLine("");
+        Console.WriteLine("Area: " + area + "cm²");
+
+        Console.WriteLine("\nDone.");
+    }
 }
